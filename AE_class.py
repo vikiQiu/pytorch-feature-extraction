2fff1128b04da68e1c1197bb5135411fc3599d67
vikiQiu <vikiqiu@hotmail.com>
2018-09-20 20:14:02 +0800

    testing

--- AE_class.py
+++ AE_class.py
@@ -183,17 +183,6 @@ def train_decoder_only(mol_short='AEClass_d', main_model=AEClass):
             eval_dir = os.path.join(evaluation_dir, 'epoch%d' % epoch)
             evaluate_cover(cover_loader, cover_sample_loader, mol, cuda, eval_dir)
 
-        # Testing on ImageNet val
-        print('######### Testing on ImageNet val Dataset ###########')
-        # test_loss_decoder, test_loss_cls, test_acc, test_top5acc = test_cls_decoder(test_loader, mol, cuda, 'Full')
-        test_loss_decoder = test_decoder(test_loader, mol, cuda, 'Full')
-        # test_loss = (1 - args.alpha) * test_loss_cls + args.alpha * test_loss_decoder / 0.001
-        writer.add_scalar('test_imagenet/loss_decoder', test_loss_decoder, epoch)
-        # writer.add_scalar('test_imagenet/loss_classifier', test_loss_cls, epoch)
-        # writer.add_scalar('test_imagenet/loss', test_loss, epoch)
-        # writer.add_scalar('test_imagenet/accuracy', test_acc, epoch)
-        # writer.add_scalar('test_imagenet/top5accuracy', test_top5acc, epoch)
-
         # Testing on Cover val
         print('######### Testing on Cover val Dataset ###########')
         # test_loss_decoder, test_loss_cls, test_acc, test_top5acc = test_cls_decoder(cover_val_loader, mol, cuda, 'Full')
@@ -204,6 +193,17 @@ def train_decoder_only(mol_short='AEClass_d', main_model=AEClass):
         # writer.add_scalar('test_cover/loss', test_loss, epoch)
         # writer.add_scalar('test_cover/accuracy', test_acc, epoch)
         # writer.add_scalar('test_cover/top5accuracy', test_top5acc, epoch)
+
+        # Testing on ImageNet val
+        print('######### Testing on ImageNet val Dataset ###########')
+        # test_loss_decoder, test_loss_cls, test_acc, test_top5acc = test_cls_decoder(test_loader, mol, cuda, 'Full')
+        test_loss_decoder = test_decoder(test_loader, mol, cuda, 'Full')
+        # test_loss = (1 - args.alpha) * test_loss_cls + args.alpha * test_loss_decoder / 0.001
+        writer.add_scalar('test_imagenet/loss_decoder', test_loss_decoder, epoch)
+        # writer.add_scalar('test_imagenet/loss_classifier', test_loss_cls, epoch)
+        # writer.add_scalar('test_imagenet/loss', test_loss, epoch)
+        # writer.add_scalar('test_imagenet/accuracy', test_acc, epoch)
+        # writer.add_scalar('test_imagenet/top5accuracy', test_top5acc, epoch)
 
         step_time = time.time()
         print('######### Training with %d batches total ##########' % len(cover_loader))
