8505ec5ecd405a66d407cfe077be0c71a8683942
vikiQiu <vikiqiu@hotmail.com>
2018-10-17 16:30:03 +0800

    print format

--- test_model.py
+++ test_model.py
@@ -172,7 +172,7 @@ def train_cls(args):
                 torch.save(mol, model_name)
                 total_tmp = total if total != 0 else 1
                 print('[Training] Epoch:', epoch, 'Step:', step, '|',
-                      'Time cost %.2f s; Classification error %.6f'
+                      'Time cost %.2f s; Classification error %.6f; '
                       'Accuracy %.3f%%; Top5 Accuracy %.3f%%' %
                       (time.time() - step_time, torch.mean(loss.data), correct * 100 / total_tmp,
                        top5correct * 100 / total_tmp))
